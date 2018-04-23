4b900ac1a2fffbfee8fea021daeb01779383c077
AzureAD\VladimirShtofmakher <Vladimirs@gmedinnovations.com>
2018-04-23 09:57:28 +0300

    login server route

--- router.js
+++ router.js
@@ -3,11 +3,12 @@ const passsportService = require('./services/passport');
 const passport = require('passport');
 
 const reqiureAuth = passport.authenticate('jwt', {session: false});
+const requireSignin = passport.authenticate('local', {session: false});
 
 module.exports = function (app) {
     app.get('/', reqiureAuth,function (req, res, next) {
         res.send(['1', '2', '3']);
     });
-
+    app.post('/signin', requireSignin, Authentication.signin);
     app.post('/signup', Authentication.signup);
 };
